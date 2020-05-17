e712a66daf6a1c14a813d29c2d2d96f8f10e903e
Abi Hunter <abihunter@gmail.com>
2020-05-16 22:04:31 -0400

    IGOR IT'S ALIVE!!

--- main.go
+++ main.go
@@ -7,20 +7,52 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
+	"path"
+	"strings"
 )
+
+func createOutfile(outfile string) *os.File {
+	f, err := os.Create(outfile)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func run(outfile *os.File, args []string)  {
+	goExecPath, err := exec.LookPath("go")
+	progArgs := []string{goExecPath, "run", outfile.Name()}
+	progArgs = append(progArgs, args...)
+	cmd := exec.Cmd{
+		Path: goExecPath,
+		Args: progArgs,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	fileName := flag.String("filename", "", "name of the file to compile")
 	flag.Parse()
+	args := flag.Args()
 	fset := poketoken.NewFileSet()
 	exp, err := pokeparser.ParseFile(fset, *fileName, nil, 0)
 	if err != nil {
 		fmt.Printf("parsing failed: %s", err)
 		return
 	}
-	err = pokeprinter.Fprint(os.Stdout, poketoken.NewFileSet(), exp)
+	fileBase := strings.TrimSuffix(*fileName, path.Ext(*fileName))
+	outfileName := fmt.Sprintf("/tmp/%s.go", fileBase)
+	outfile := createOutfile(outfileName)
+	err = pokeprinter.Fprint(outfile, poketoken.NewFileSet(), exp)
 	if err != nil {
 		fmt.Printf("output failed: %s", err)
 	}
+	run(outfile, args)
 	fmt.Printf("\n")
 }
